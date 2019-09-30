e977fbb23126f291957e32dfd1ced2195a76ef1a
lijianjian <you@example.com>
2019-09-30 15:10:49 +0800

    headers

--- middleware/user-agent.js
+++ middleware/user-agent.js
@@ -1,9 +1,9 @@
 import {isPc} from '~/utils'
 
 export default function (context) {
-  context.userAgent = process.server ? context.req.headers['user-agent'] : navigator.userAgent
-  const pc = isPc(context.userAgent)
-  if(!pc) {
-    context.redirect('/mobile')
-  }
+  // context.userAgent = process.server ? context.req.headers['user-agent'] : navigator.userAgent
+  // const pc = isPc(context.userAgent)
+  // if(!pc) {
+  //   context.redirect('/mobile')
+  // }
 }
